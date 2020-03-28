193843a9b5a1d17440e54b6afe30c6374cfa1d96
Andrey Martunyk <aniks.zoom@gmail.com>
2020-03-28 18:13:07 +0200

    task is ready

--- app2.js
+++ app2.js
@@ -16,27 +16,23 @@ myBinder.bind("#enddate", model, "enddate");
 myBinder.bind("#room", model, "room");
 myBinder.bind("#sum", model, "sum");
 
-document.querySelector("button").addEventListener("click", e => {
-  if (model.startdate && model.enddate && model.room) {
-    console.log("if");
-
-    const days = Math.round(
-      (new Date(model.enddate) - new Date(model.startdate)) /
-        (24 * 60 * 60 * 1000)
-    );
-    if (pricing[model.room]) {
-      console.log("sum");
-
-      model.sum = pricing[model.room] * days;
-    }
-  }
-  else {
-      model.sum = 'fill all the fields'
-  }
-});
+// document.querySelector("button").addEventListener("click", calc);
 
 function calc(){
-    console.log("calc");
+    if (model.startdate && model.enddate && model.room) {
+        console.log("if");
+    
+        const days = Math.round(
+          (new Date(model.enddate) - new Date(model.startdate)) /
+            (24 * 60 * 60 * 1000)
+        );
+        if (pricing[model.room]) {   
+          model.sum = pricing[model.room] * days;
+        }
+      }
+      else {
+          model.sum = 'fill all the fields'
+      }
     
 }
 
